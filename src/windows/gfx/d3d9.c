fd40f9881e35e386f0b7bf295b03c340a19ea2b5
Christopher Dickson <chrisd1100@gmail.com>
2021-03-08 11:43:28 -0500

    copyright updates

--- src/windows/gfx/d3d9.c
+++ src/windows/gfx/d3d9.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Christopher D. Dickson <cdd@matoya.group>
+// Copyright (c) Christopher D. Dickson <cdd@matoya.group>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
